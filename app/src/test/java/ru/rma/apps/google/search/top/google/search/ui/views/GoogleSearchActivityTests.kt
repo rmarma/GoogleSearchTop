3bf1c292584e8e766bacbb5ef8cfd0ba0823f69e
RMA <8e10001@gmail.com>
2018-11-28 10:39:06 +0300

    View tests

--- app/src/test/java/ru/rma/apps/google/search/top/google/search/ui/views/GoogleSearchActivityTests.kt
+++ app/src/test/java/ru/rma/apps/google/search/top/google/search/ui/views/GoogleSearchActivityTests.kt
@@ -1,10 +1,21 @@
 package ru.rma.apps.google.search.top.google.search.ui.views
 
+import android.widget.ImageButton
+import com.google.android.material.textfield.TextInputEditText
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import ru.rma.apps.google.search.top.R
 
 @RunWith(RobolectricTestRunner::class)
 class GoogleSearchActivityTests {
 
+    private val activity = Robolectric.setupActivity(GoogleSearchActivity::class.java)
 
+    @Test
+    fun activity_requireViews() {
+        activity.requireViewById<TextInputEditText>(R.id.inputTextSearch)
+        activity.requireViewById<ImageButton>(R.id.buttonSearch)
+    }
 }
